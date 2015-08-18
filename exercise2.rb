34dd077ad2254676c8acb16356f74e8971326357
Henry Lan <henrylan@Henrys-MacBook-Air.local>
2015-08-18 15:34:56 -0400

    Exercise 2 comoplete

---
+++ exercise2.rb
@@ -0,0 +1,22 @@
+#Tip for $55 meal
+
+meal = 55
+tip = 55 * 0.15
+puts "A good tip for a $55 meal is $#{tip}"
+
+#Adding a string and integer
+
+integer = 5
+string  = "Hello!"
+
+# sum = integer + string
+puts "The sum of an integer and string is" + integer.to_s + string
+
+int1 = 45628
+int2 = 7839
+
+puts "The product of the two integers would be #{int1 * int2}"
+
+a = (true && false) || (false && true) || !(false && false)
+puts a
+
